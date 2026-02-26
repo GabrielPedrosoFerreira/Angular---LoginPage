70f714e1a182f40d2b3a4944b492063875edd4e5
Gabriel Pedroso Ferreira <gabrielpedroso@wmi.solutions>
2026-02-26 00:25:47 -0300

    Implement initial layout and styles; update app components and assets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LinkButton } from './components/link-button/link-button';
+import { Input } from './components/input/input';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LinkButton, Input, ReactiveFormsModule, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Login');
+  title = 'login';
+
+  protected form!: FormGroup;
+  protected emailField!: any
+  protected passwordField!: any
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      emailField: [null, [Validators.required, Validators.email]],
+      passwordField: [null, [Validators.required, Validators.minLength(6)]]
+    })
+
+    this.emailField = this.form.get('emailField');
+    this.passwordField = this.form.get('passwordField');
+  }
+
+  protected submit() {
+    if(this.form.invalid)return;
+
+    //const emailValue = this.form.get("emailField");
+    //const passwordValue = this.form.get("passwordField");
+    //console.log(emailValue?.value);
+    //console.log(passwordValue?.value);
+    //console.log(this.form.getRawValue());
+    const {emailField, passwordField} = this.form.getRawValue();
+    //console.log(emailField);
+    //console.log(passwordField);
+
+    console.log(`Logando usuario... email: ${emailField} password: ${passwordField}`);
+  }
 }
---
+++ src/app/components/link-button/link-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a[link-button]',
+  imports: [],
+  template: '<ng-content/>',
+  styleUrl: './link-button.scss',
+})
+export class LinkButton {
+
+}
